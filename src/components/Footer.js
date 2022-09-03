18cf2d036125c5887e51a3db6f46b8236ba26294
tezadaconnect <terrencecalzada@gmail.com>
2022-09-04 01:20:59 +0800

    Modify footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,89 +1,82 @@
-import React from 'react';
-import facebookIcon from '../assets/facebookIcon.png';
-import instagramIcon from '../assets/instagramIcon.png';
+import React from "react";
+import { AiFillFacebook, AiFillInstagram } from "react-icons/ai";
+import { outsiteWebHelper } from "../router/outer_router";
 const Footer = () => {
-	return (
-		<div>
-			<div>
-				<div className="flex flex-row justify-center text-white bg-green-600 gap-24 pt-2 font-inter">
-					<div>
-						<div>
-							<div className=" flex w-96 flex-col">
-								<div>
-									<h1 className="font-bold text-lg ">About Ayungon</h1>
-								</div>
-								<p className="flex justify-content text-sm text-justify">
-									Lorem Ipsum is simply dummy text of the printing and
-									typesetting industry. Lorem Ipsum has been the industry's
-									standard dummy text ever since the 1500s, when an unknown
-									printer took a galley of type and scrambled it to make a type
-									specimen book. It has survived not only five centuries, but
-									also the leap into electronic typesetting, remaining
-									essentially unchanged. It was popularised in the{' '}
-								</p>
-							</div>
-						</div>
-					</div>
-					<div>
-						<div className=" flex w-80 flex-col text-sm">
-							<div>
-								<h1 className="font-bold text-lg ">Location </h1>
-							</div>
-							<div className="flex justify-content flex-col">
-								<div>
-									<b>Address:</b> Poblacion, Ayungon Negros Oriental,
-									Philippines 6210
-								</div>
+  return (
+    <div className="text-white bg-green-600 font-inter">
+      <div className="flex lg:flex-row flex-col lg:w-8/12 w-10/12 m-auto justify-between py-10 gap-10">
+        <div className="flex lg:w-96 flex-col">
+          <div>
+            <h1 className="font-bold text-2xl mb-4">About Ayungon</h1>
+          </div>
+          <p className="flex text-sm text-justify">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the
+          </p>
+        </div>
 
-								<div>
-									<b>Phone:</b> 0917 - 3200 - 557 (035) - 400 - 5276
-								</div>
-								<div>
-									<b>Email:</b>ayungon_mayorsoffice@yahoo.com
-								</div>
-							</div>
-						</div>
-					</div>
-					<div className=" flex flex-col text-sm ">
-						<div>
-							<div className="flex flex-row justify-start pl-2 gap-5 items-center">
-								<div>
-									<img
-										className="w-10"
-										src={facebookIcon}
-										alt="facebook Icon"
-									/>
-								</div>
-								<div>
-									<a href="https://web.facebook.com/municipalityofayungon">
-										facebook.com/municipalityofayungon
-									</a>
-								</div>
-							</div>
-						</div>
-						<div>
-							<div>
-								<div className="flex flex-row justify-center gap-3 items-center">
-									<div>
-										<img
-											className="w-14"
-											src={instagramIcon}
-											alt="facebook Icon"
-										/>
-									</div>
-									<div>
-										<a href="https://www.instagram.com/municipalityofayungon/">
-											instagram.com/municipalityofayungon/
-										</a>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+        <div className="flex flex-col text-sm">
+          <div>
+            <h1 className="font-bold text-2xl mb-4">Location </h1>
+          </div>
+          <div className="flex flex-col justify-start gap-3">
+            <div>
+              <strong>Address:</strong> Poblacion, Ayungon Negros Oriental,
+              Philippines 6210
+            </div>
+
+            <div>
+              <strong>Phone:</strong> 0917 - 3200 - 557 (035) - 400 - 5276
+            </div>
+            <div>
+              <strong>Email:</strong> ayungon_mayorsoffice@yahoo.com
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div>
+            <h1 className="font-bold text-2xl mb-4">Social Media</h1>
+          </div>
+          <div className="flex flex-col text-sm gap-3">
+            {socialMediaAccounts?.map((item, key) => {
+              return (
+                <div
+                  key={key}
+                  onClick={() => outsiteWebHelper(item.link)}
+                  className="flex flex-row gap-3 items-center"
+                >
+                  <div>{item.icon}</div>
+                  <div className="cursor-default hover:underline">
+                    {item.label}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
+
+const socialMediaAccounts = [
+  {
+    label: "@municipalityofayungon",
+    link: "https://web.facebook.com/municipalityofayungon",
+    icon: <AiFillFacebook size={30} />,
+    alt: "facebook",
+  },
+  {
+    label: "@municipalityofayungon",
+    link: "https://www.instagram.com/municipalityofayungon/",
+    icon: <AiFillInstagram size={30} />,
+    alt: "instagram",
+  },
+];

da9516ea106ac158ebf46dcef461ebf0c4b0f49b
v1rm <105407091+v1rm@users.noreply.github.com>
2022-11-15 15:15:54 +0800

    ContactsFooter

--- src/views/Contacts.js
+++ src/views/Contacts.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-
+import Footer from '../components/Footer';
 const Contacts = () => {
 	return (
 		<div>
+			<div className="w-screen h-96"></div>
 			<Outlet />
+			<Footer />
 		</div>
 	);
 };

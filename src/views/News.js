ee3414d49af7fc992a3b859ae9cca2f5c76c2ae9
v1rm <105407091+v1rm@users.noreply.github.com>
2022-11-24 08:46:20 +0800

    news update

--- src/views/News.js
+++ src/views/News.js
@@ -53,8 +53,10 @@ const News = () => {
 				<div className="flex items-center flex-col ">
 					<div className="drop-shadow-2xl">
 						<Iframe
-							src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fprofile.php%3Fid%3D100087737546288&tabs=timeline&width=500&height=120&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
-							className="lg:w-[500px] w-[400px] h-96"
+							src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fprofile.php%3Fid%3D100087737546288&tabs=timeline&width=500&height=600&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+							width="500"
+							height="400"
+							className="border:none;overflow:hidden"
 							scrolling="no"
 							frameborder="0"
 							allowfullscreen="true"
